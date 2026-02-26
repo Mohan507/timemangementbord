4c09a3b464bb10fa5abb5162445d4460b16b4221
Mohan507 <chitukulamohan507@gmail.com>
2026-02-26 18:39:47 +0530

    bug fixed

--- app/api/timesheets/route.ts
+++ app/api/timesheets/route.ts
@@ -1,19 +1,47 @@
-import { NextResponse } from "next/server"
+// import { NextResponse } from "next/server"
+// import { mockTimesheets } from "../../lib/mockTimesheets"
+
+// export async function GET() {
+//   return NextResponse.json(mockTimesheets)
+// }
+
+// export async function POST(req: Request) {
+//   const body = await req.json()
+
+//   const newEntry = {
+//     id: Date.now(),
+//     ...body
+//   }
+
+//   mockTimesheets.push(newEntry)
+
+//   return NextResponse.json(newEntry)
+// }
+import { NextRequest, NextResponse } from "next/server"
 import { mockTimesheets } from "../../lib/mockTimesheets"
 
-export async function GET() {
-  return NextResponse.json(mockTimesheets)
-}
+export async function PUT(
+  request: NextRequest,
+  context: { params: Promise<{ id: string }> }
+) {
+  const { id } = await context.params
+  const body = await request.json()
 
-export async function POST(req: Request) {
-  const body = await req.json()
+  const index = mockTimesheets.findIndex(
+    (item) => item.id === Number(id)
+  )
 
-  const newEntry = {
-    id: Date.now(),
+  if (index === -1) {
+    return NextResponse.json(
+      { message: "Not found" },
+      { status: 404 }
+    )
+  }
+
+  mockTimesheets[index] = {
+    ...mockTimesheets[index],
     ...body
   }
 
-  mockTimesheets.push(newEntry)
-
-  return NextResponse.json(newEntry)
+  return NextResponse.json(mockTimesheets[index])
 }
